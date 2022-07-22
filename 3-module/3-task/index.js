7d1a888fabb955d7515b4756c1ac7c6a65070184
TraktorT330 <sport11b@ya.ru>
2022-07-22 09:50:24 +0300

    1-5 задачи модуль 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -8,3 +8,15 @@ function camelize(str) {
   let res = a[0]+b.map(handler).join("") ;
   return res
 }
+
+// Второй способ
+// function camelize(str) {
+//   let arr = str.split("").map((element, index, arr) => {
+//     if (element === "-") return;
+//     if (arr[index - 1] === "-") return element.toUpperCase();
+//
+//     return element;
+//   });
+//
+//   return arr.join("");
+// }
