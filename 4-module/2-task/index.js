89e102a5f9be7e1906e1f0df690c916a5edacbad
TraktorT330 <sport11b@ya.ru>
2022-07-24 16:37:11 +0300

    1  задача модуль 4 Прошу пояснить, почему не проходит первый способ решения в скрипте index.js . Спасибо. p/s в index.html добавлено <meta charset="UTF-8">
    2 задача модуль 4 решение

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,14 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  // получаем коллекцию строк table.rows и пробегаем по каждой строке в цикле
+  for (let tr of table.rows) {
+    let i = tr.rowIndex; // получили номер строки
+    // получаем коллекцию ячеек tr.cells и пробегаем по каждой ячейке в цикле
+    for (let td of tr.cells){
+      let j = td.cellIndex // получили номер столбца
+      // если индекс строки === индексу столбца, то меняем стиль заливки на красный
+      if( i===j) {console.log(i , j);
+        table.rows[i].cells[j].style.backgroundColor = 'red'
+      }
+    }
+  }
 }
