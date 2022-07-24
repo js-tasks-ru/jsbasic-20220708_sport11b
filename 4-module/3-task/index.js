2f4e68144f75b9068d1f9de0ee48dcc16ca34ec9
TraktorT330 <sport11b@ya.ru>
2022-07-24 21:48:50 +0300

    1  задача модуль 4 Прошу пояснить, почему не проходит первый способ решения в скрипте index.js . Спасибо. p/s в index.html добавлено <meta charset="UTF-8">
    2 задача модуль 4 решение
    3 задача модуль 4 решение

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,18 @@
 function highlight(table) {
-  // ваш код...
-}
+
+  // получаем коллекцию строк table.rows и пробегаем по каждой строке в цикле
+  for (let tr of table.rows) {
+    // получаем значение нестанлартного атрибута data-available
+    if (tr.cells[3].dataset.available === 'true'){
+      // устанавливаем класс
+      tr.classList.add("available")}
+    else if (tr.cells[3].dataset.available === 'false') {tr.classList.add("unavailable")}
+    // если атрибут data-available неопределен , то добавляем атрибут hidden
+    else {tr.setAttribute('hidden','hidden')};
+
+    if (tr.cells[2].innerHTML === 'f') {tr.classList.add("female")}
+    else {tr.classList.add("male")};
+
+    // устанавливаем инлайн-стиль
+    if (tr.cells[1].innerHTML < '18') {tr.style.textDecoration = 'line-through'};
+  }}
