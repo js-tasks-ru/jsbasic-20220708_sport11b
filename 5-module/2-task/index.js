d1c3cd5182ad391b419b5a9ec572ec93c3a247cf
TraktorT330 <sport11b@ya.ru>
2022-07-31 15:42:55 +0300

    1  задача модуль 4 решение 2 задача модуль 4 решение 3 задача модуль 4 решение
    1 задача модуль 5 решение
    2 задаче модуль 5 решение

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  // находим наш div c id = text
+  let dv = document.querySelector("#text");
+  // функция обработчик для document.addEventListener
+  function handler (event) {
+    // если событие случилось на элементе у которого class содержит toggle-text-button
+    if (event.target.classList.contains('toggle-text-button'))
+    {
+      dv.hidden = !dv.hidden;  //  то у тега div устанавливаем атрибут hidden или наоборот
+    }
+
+  }
+  // добавили обработчик события на нажатие кнопки
+  document.addEventListener("click", handler);
 }
